2de6dc783bb23abc1a6831d169ac166bbf4f039d
saikatkar <saikat16th@gmail.com>
2019-12-03 20:50:50 +0530

    graph problems

--- java/graph/ToplogicalSort.java
+++ java/graph/ToplogicalSort.java
@@ -12,6 +12,8 @@ The first vertex in topological sorting is always a vertex with in-degree as 0 (
 Topological Sorting vs Depth First Traversal (DFS):
 In DFS, we print a vertex and then recursively call DFS for its adjacent vertices. In topological sorting, we need to print a vertex before its adjacent vertices. For example, in the given graph, the vertex ‘5’ should be printed before vertex ‘0’, but unlike DFS, the vertex ‘4’ should also be printed before vertex ‘0’. So Topological sorting is different from DFS. For example, a DFS of the shown graph is “5 2 3 1 0 4”, but it is not a topological sorting
  * @author saikatkar
+ * 
+ * 
  *
  */
 public class ToplogicalSort {
